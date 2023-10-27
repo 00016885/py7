fed7fc84fb8d0a04e0c94ef7243a451ae169077f
00016885 <wiuts11@gmail.com>
2023-10-27 14:23:02 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+
+student = {}
+
+print(student)
+
+student = {'name': 'Lola',
+           'age': 19,
+           'course': 'BIS'
+           }
+
+print(student)
+
+student1 = dict()
+student1['name'] = 'Ahmad'
+student1['age'] = 25
+student1['course'] = 'BIS'
+student1['married'] = False
+
+print(len(student))
+
+print('Lola' in student['name'])
+values = student1.values()
+print(25 in values)
+
